d680d34a861a00871ff0d3794b9fdb646309cf59
layanmoyura <layanmoyura@gmail.com>
2025-02-20 20:38:45 +0530

    db setup done

--- DbContext.cs
+++ DbContext.cs
@@ -1,6 +1,9 @@
-﻿namespace TestSPA
+﻿using Microsoft.EntityFrameworkCore;
+using TestSPA.Models;
+
+public class AppDbContext : DbContext
 {
-    public class DbContext
-    {
-    }
+    public DbSet<Product> Products { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
